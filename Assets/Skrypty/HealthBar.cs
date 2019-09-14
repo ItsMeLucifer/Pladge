060ae312ee5b6ef8ced6725097cab42ce405e73b
ItsMeLucifer <31631615+ItsMeLucifer@users.noreply.github.com>
2019-09-14 14:18:43 +0200

    Update v1.0
    Skrypt do Health Bara, i usunięcie niepotrzebnych śmieci z projektu.

---
+++ Assets/Skrypty/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject bar;
+    public GameObject rock;
+    public GameObject text;
+    public GameObject czerw;
+    public int health=100;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+    }
+}
