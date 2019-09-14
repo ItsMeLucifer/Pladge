ac69ac9ec22b14ec9285421b1086587b726441da
ItsMeLucifer <31631615+ItsMeLucifer@users.noreply.github.com>
2019-09-14 18:57:38 +0200

    Update v1.1
    Dodalem system pociskow i wszystko cacy, poza kurwa faktem, ze nie moge zmienic kąta wystrzału pocisków na randomowy, bo chuj wie czemu. I ciagle w jednej linii zapierdalają jak pojebane. W skryptcie "enemy" na samym dole masz kurwa komentarz, chuj wie - może coś zdziałasz

--- Assets/Skrypty/enemy.cs
+++ Assets/Skrypty/enemy.cs
@@ -7,12 +7,15 @@ public class enemy : MonoBehaviour
 {
     public GameObject monster;
     public float speedo = 0.1f;
-    bool pow=true;
+    bool pow = true;
     public float rotation;
-    
+    int zakres=0;
+    public int random;
+
     void Start()
     {
         monster.transform.localScale = new Vector3(0, 0, 0);
+        InvokeRepeating("Atak", 2.0f, 0.5f);
     }
 
     void Update()
@@ -24,13 +27,13 @@ public class enemy : MonoBehaviour
         }
         if (monster.transform.rotation.eulerAngles.z <= 354 && monster.transform.rotation.eulerAngles.z > 100)
         {
-            speedo *=-1;
+            speedo *= -1;
         }
         else if (monster.transform.rotation.eulerAngles.z >= 6 && monster.transform.rotation.eulerAngles.z < 100)
         {
-            speedo*=-1;
+            speedo *= -1;
         }
-        if (GameObject.Find("blackhole").GetComponent<czarnadziura>().powiekszanie == false && pow==true)
+        if (GameObject.Find("blackhole").GetComponent<czarnadziura>().powiekszanie == false && pow == true)
         {
             monster.transform.localScale += new Vector3(0.02f, 0.02f, 0);
         }
@@ -38,5 +41,20 @@ public class enemy : MonoBehaviour
         {
             pow = false;
         }
+        //STRZALY SKALNE
+        
+        
+        
+    }
+    public GameObject rockbullet;
+    public Transform firepoint;
+    void Atak()
+    {
+        //NO KURWA MAC NIE WIEM JAK ZROBIC ABY KURWA RANDOMOWO DO CHUJA WYBIERALO KĄT WYSTRZAŁU, NO JEBNE ZARAZ
+        System.Random rand = new System.Random();
+        random = rand.Next(360);
+        Quaternion qua = new Quaternion(0,0, random,0);
+        Instantiate(rockbullet, firepoint.position, qua);//KURWA TUTAJ< 3 ZMIENNA W NAWIASIE MUSI BYC JAKIMS JEBANYM QUATERNIONEM NO JA PIERDOLE ZAJEBIEKOMUS ZARAZ, JAK SIE ZTEGO GOWNA KORZYSTA
+        //CHCE TYLKO ABY ZMIENNA RANDOM BYLA KURWA W ROTACJI Z, TAK KURWA TRUDNO?
     }
 }
---
+++ Assets/bulleto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulleto : MonoBehaviour
+{
+    public float speed = 20f;
+    public int damage = 10;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.name == "platforma")
+        {
+            GameObject.Find("LifeBar").GetComponent<HealthBar>().health-=damage;
+        }
+        Destroy(gameObject);
+    }
+}
