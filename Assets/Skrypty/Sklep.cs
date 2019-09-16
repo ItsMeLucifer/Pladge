ce41ade3dce4fdc72f83fcd627a3cd1e84e720de
ItsMeLucifer <31631615+ItsMeLucifer@users.noreply.github.com>
2019-09-16 10:25:08 +0200

    Update v1.5
    Dodałem nową scenę, w ktorej bedzie sklep z gadzetami i innymi pierdołami, na razie działa kod, aby po kliknieciu coin'a - przenosiło do nowej sceny. Jednak działa tylko na dotyk, a nie na myszke, wiec na kompie chuja bedzie działało xD

---
+++ Assets/Skrypty/Sklep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sklep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                if (raycastHit.collider.name == "coin")
+                {
+                    SceneManager.LoadScene(1);
+                }
+            }
+        }
+    }
+}
