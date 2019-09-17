ee09ae82ff510bcbc67fbaf229aa226612d9172c
NukeeMann <49735204+NukeeMann@users.noreply.github.com>
2019-09-17 18:29:19 +0200

    Update
    Dodałem UI pauzy i teraz można swobodnie bez bugów poruszać się między Menu - Poziom1

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    
+    public void Pause(bool pauza)
+    {
+        if(pauza)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+}
