6a29ae40b8fba3ad258ea025c4f5acc377495ea9
ItsMeLucifer <31631615+ItsMeLucifer@users.noreply.github.com>
2019-09-30 20:50:14 +0200

    Update v2.1

---
+++ Assets/Skrypty/Konestelacje/CassiopeiAnim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CassiopeiAnim : MonoBehaviour
+{
+    public Animator animator;
+    public int level=1;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        animator.SetInteger("level", level);
+        
+    }
+}
